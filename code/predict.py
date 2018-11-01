1f3f82654d0d2cf921ee62b2a0c261f1c13cf607
Unknown <srb2eyeshield@gmail.com>
2018-11-01 17:17:45 +0100

    random forest en commentaire + fix log-loss
    j'crois

--- code/predict.py
+++ code/predict.py
@@ -1,10 +1,11 @@
+import numpy as np
 
 from read_data import *
 
 from sklearn.metrics import log_loss
 
 
-file = "../train.csv"
+file = "../../train.csv"
 data = 	prepare(file)
 
 #data = full_train(file)
@@ -23,16 +24,27 @@ from sklearn.neural_network import MLPClassifier
 gnb = MLPClassifier(solver='lbfgs', alpha=1e-5,  hidden_layer_sizes=(70,50, 30), random_state=1)
 
 
+
+
 gnb = GaussianNB()
 
 y_pred = gnb.fit(data[0][0], list(data[0][1])).predict(data[1][0])
+y_pred_proba = gnb.fit(data[0][0], list(data[0][1])).predict_proba(data[1][0])
+
+
+#### Random Forest
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.datasets import make_classification
+
+#rndf = RandomForestClassifier(n_estimators=100, max_depth=20,random_state=0)
+#y_pred = rndf.fit(data[0][0], list(data[0][1])).predict(data[1][0])
 
 
 #y_pred = gnb.fit(data[0], list(data[1])).predict(pr)
+####
 
 #write_output(y_pred)
-#print("Number of mislabeled points out of a total %d points : %d" % (len(data[1][1],(data[1][1] != y_pred).sum())))
-#print(log_loss(data[1][1],y_pred))
+
 
 score = (data[1][1] != y_pred).sum()
 print("erreur : ", score/len(data[1][1]))
@@ -44,17 +56,27 @@ def to_class(l):
 		out[i] =  int(l[i][-1])-1
 	return out
 
+def class_to_number(class_list):
+    number_list = np.zeros([len(class_list)])
+    for i in range(len(class_list)):
+        number_list[i] = int(class_list[i][-1:])-1
+    return number_list
 
-import numpy as np
 
-def multiclass_log_loss(y_true, y_pred, eps=1e-15):
-	predictions = np.clip(y_pred, eps, 1 - eps)
+def multiclass_log_loss(y_true, y_predict, eps=1e-15):
+    predictions = np.clip(y_predict, eps, 1 - eps)
+    # normalize row sums to 1
+    predictions /= predictions.sum(axis=1)[:, np.newaxis]
 
-	# normalize row sums to 1
-	predictions /= predictions.sum(axis=1)[:, np.newaxis]
+    actual = np.zeros(y_predict.shape)
+    rows = actual.shape[0]
+    actual[np.arange(rows), y_true.astype(int)] = 1
+    vsota = np.sum(actual * np.log(predictions))
+    return -1.0 / rows * vsota
 
-	actual = np.zeros(y_pred.shape)
-	rows = actual.shape[0]
-	actual[np.arange(rows), y_true.astype(int)] = 1
-	vsota = np.sum(actual * np.log(predictions))
-	return -1.0 / rows * vsota
+
+
+y_true_temp = data[1][1]
+y_true = class_to_number(y_true_temp)
+print("Number of mislabeled points out of a total %d points : %d" % ( len(data[1][1]),(data[1][1] != y_pred).sum()))
+print(multiclass_log_loss(y_true,y_pred_proba))
